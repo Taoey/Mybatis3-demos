26be72106915fe1fdf1d176b7aa4873485055c40
taoey <741494582@qq.com>
2018-02-10 15:55:34 +0800

    20180210-1554:补充数据库文件

---
+++ dbbook.sql
@@ -0,0 +1,131 @@
+-- MySQL dump 10.13  Distrib 5.7.21, for Linux (x86_64)
+--
+-- Host: localhost    Database: dbbook
+-- ------------------------------------------------------
+-- Server version	5.7.21-0ubuntu0.16.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `c`
+--
+
+DROP TABLE IF EXISTS `c`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `c` (
+  `_c` varchar(10) NOT NULL,
+  `cName` varchar(10) DEFAULT NULL,
+  `examDate` datetime DEFAULT NULL,
+  `_t` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`_c`),
+  KEY `T#` (`_t`),
+  CONSTRAINT `c_ibfk_1` FOREIGN KEY (`_t`) REFERENCES `t` (`_t`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `c`
+--
+
+LOCK TABLES `c` WRITE;
+/*!40000 ALTER TABLE `c` DISABLE KEYS */;
+INSERT INTO `c` VALUES ('c1','数据库','2017-12-01 19:53:18','t1'),('c2','编译原理','2018-01-07 19:53:35','t2'),('c3','java web','2018-01-16 19:53:44','t1');
+/*!40000 ALTER TABLE `c` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `s`
+--
+
+DROP TABLE IF EXISTS `s`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `s` (
+  `_s` varchar(10) NOT NULL DEFAULT '',
+  `sName` varchar(10) DEFAULT NULL,
+  `age` int(11) DEFAULT NULL,
+  `sex` int(11) DEFAULT NULL,
+  PRIMARY KEY (`_s`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `s`
+--
+
+LOCK TABLES `s` WRITE;
+/*!40000 ALTER TABLE `s` DISABLE KEYS */;
+INSERT INTO `s` VALUES ('s1','提莫',18,1),('s2','小炮',17,0),('s3','鳄鱼',30,1),('s4','风女',28,0),('s5','吸血鬼',28,1);
+/*!40000 ALTER TABLE `s` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sc`
+--
+
+DROP TABLE IF EXISTS `sc`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sc` (
+  `_s` varchar(10) NOT NULL DEFAULT '',
+  `_c` varchar(10) NOT NULL DEFAULT '',
+  `score` int(11) DEFAULT NULL,
+  PRIMARY KEY (`_s`,`_c`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sc`
+--
+
+LOCK TABLES `sc` WRITE;
+/*!40000 ALTER TABLE `sc` DISABLE KEYS */;
+INSERT INTO `sc` VALUES ('s1','c1',98),('s1','c2',89),('s2','c1',75),('s2','c3',100);
+/*!40000 ALTER TABLE `sc` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `t`
+--
+
+DROP TABLE IF EXISTS `t`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `t` (
+  `_t` varchar(10) NOT NULL DEFAULT '',
+  `tName` varchar(10) DEFAULT NULL,
+  `title` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`_t`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `t`
+--
+
+LOCK TABLES `t` WRITE;
+/*!40000 ALTER TABLE `t` DISABLE KEYS */;
+INSERT INTO `t` VALUES ('t1','Marry','主任'),('t2','Tom','教授'),('t3','White','学士');
+/*!40000 ALTER TABLE `t` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-02-10 15:53:04
